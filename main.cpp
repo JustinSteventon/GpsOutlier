780ac25a394343082400aff0a4693c626ddf54df
Phillip Maluta <phillip.maluta@volia-software.com>
2015-11-03 01:41:26 +0200

    removed old files

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Outlier.hpp"
-
-int main(int argc, char *argv[])
-{
-  char* filename = argv[1];
-  Outlier* outlier = new Outlier(filename, 1000);
-  outlier->normalize();
-  return 0;
-}
